98d510ca99238e9e63810e07f59d2c57b51fcf6d
Ashok <ashokitschool@gmail.com>
2026-02-12 06:53:26 +0530

    constructor added

--- Demo.java
+++ Demo.java
@@ -1,15 +1,16 @@
 public class Demo{
+
+
+	public Demo(){
+
+	}
 	
 	// JIRA STory-101
 	public void m1(){
 		// logic
 	}
 
-    int i = 10;
-  int j = 20;
-
-
-	// JIRA STORY - 102
+ 	// JIRA STORY - 102
 	public void m2(){
 		// logic
 	}
